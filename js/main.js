a1a400053add81a1608e94be6f9b826ea416d701
priya-nusrat18 <nusratjahan5552@gmail.com>
2021-02-07 18:26:04 +0600

    was little bit difficult

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const search = document.getElementById('search');
+const term = search.value ;
+search.addEventListener('click', function () {
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+    .then(res => res.json())
+    .then(data => displayMeal(data))
+    .catch(error => alert('you type wrong mealname'))
+
+
+});
+
+const displayMeal = foods =>{
+    const mealDiv = document.getElementById('list-single-box');
+    foods.meals.forEach(meal => {
+        const foodDiv = document.createElement('div');
+        foodDiv.className = 'food';
+
+        const foodInfo =`
+        <img onclick="displayDetail('${meal.idMeal}')"  class="food-img" src="${meal.strMealThumb}">
+        <p class="food-name">${meal.strMeal}</p>
+        `
+        foodDiv.innerHTML=foodInfo;
+        mealDiv.appendChild(foodDiv);
+    });
+}
+
+const displayDetail = idMeal =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => renderInfo(data.meals[0]));
+}
+const renderInfo = meal => {
+    const foodsDiv = document.getElementById('meal-detail')
+    foodsDiv.innerHTML =`
+    <img  class="detail-food-img" src ="${meal.strMealThumb}">
+    <h1>${meal.strMeal}</h1>
+    <h3>Ingredient</h3>
+    <p>1- ${meal.strIngredient1} ${meal.strMeasure1}</p>
+    <p>2-  ${meal.strIngredient2} ${meal.strMeasure2}</p>
+    <p>3-  ${meal.strIngredient3} ${meal.strMeasure3}</p>
+    <p>4-  ${meal.strIngredient4} ${meal.strMeasure4}</p>
+    <p>5- ${meal.strIngredient5} ${meal.strMeasure5}</p>
+    <p>6-  ${meal.strIngredient6} ${meal.strMeasure6}</p>
+
+    `
+}
